aec128ea52beb896898c8b6366dfff711b909110
Fatima <fatima.hammouri@gmail.com>
2021-01-22 04:52:53 +0000

    added pseudocode

--- game.py
+++ game.py
@@ -1,7 +1,25 @@
 """A number-guessing game."""
+import random
 
 # Put your code here
 player_name = input ("Howdy, what's your name?")
-print("Hi " + player_name + " Welcome to the Guessing game")
+print(f"Hi {player_name} Welcome to the Guessing game")
+random_number = int(random.randint(1, 100))
+#print(random_number)  this is to show what random number was generated 
+print (player_name + " ,I'm thinking of a number between 1 and 100.\nTry to guess my number.")
 
+number_attempts = 0
+
+
+while True:
+    guessed_number = input ("what is your guess? ")
+    number_attempts += 1 
+    if guessed_number == random_number:
+        print(f"Well done, {player_name}! You found my number in {number_attempts} tries!")
+    
+    if guessed_number > random_number:
+        print("you're guess is too high")
+        int(input())
+
+        continue
 
---
+++ my_game.py
@@ -0,0 +1,10 @@
+#get user name
+#salute user 
+#get user name
+#generate random_number
+#create a counter
+#get user guess
+#add counter by 1
+#if the_guess is == random_number --> print "well done {} you found my number in counter"
+#if the_guess is > random_number ---> print "your guess is too high, try again"
+#if the_guess is < random_number ---> print "your guess is too low, try again"
